6de7f48e850203708b32cbce4b5110d9260df92f
solok18 <ksolorzano862@gmail.com>
2025-07-11 22:05:36 -0700

    assigment and sql code added

--- database/projectone-sql-code.sql
+++ database/projectone-sql-code.sql
@@ -243,3 +243,13 @@ VALUES   (
     'White',
     5
   );
+
+
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
