9481641768cae3ba584a629928b42505b1bc13f5
solok18 <ksolorzano862@gmail.com>
2025-07-23 22:48:56 -0700

    week 4 activities

---
+++ models/account-model.js
@@ -0,0 +1,17 @@
+const pool = require("../database/")
+
+/* *****************************
+*   Register new account
+* *************************** */
+async function registerAccount(account_firstname, account_lastname, account_email, account_password){
+  try {
+    const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'client') RETURNING *"
+    return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
+  } catch (error) {
+    return error.message
+    // console.error(" Registration INSERT error:", error);
+    // throw error;
+  }
+}
+
+module.exports = {registerAccount};
--- server.js
+++ server.js
@@ -13,6 +13,38 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+// Express Message Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+//body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true})) // for parsing application/x-www-form-urlencoded
+
 
 /* ***********************
  * View Engine and Templates
@@ -33,6 +65,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory Route
 app.use("/inv", inventoryRoute)
+
+// Login route
+app.use("/account", accountRoute)
 
 //error route
 app.get("/error/trigger-error", (req, res, next) =>{
